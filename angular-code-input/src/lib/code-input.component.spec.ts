ebf2f51cc5bc8ce4e431919cb5a4c4b4be9abe70
Alex D <alex.miniapps@gmail.com>
2021-05-26 23:25:54 +0300

    Replaced deprecated async() to waitForAsync() in tests

--- angular-code-input/src/lib/code-input.component.spec.ts
+++ angular-code-input/src/lib/code-input.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CodeInputComponent } from './code-input.component';
 
@@ -6,7 +6,7 @@ describe('CodeInputComponent', () => {
   let component: CodeInputComponent;
   let fixture: ComponentFixture<CodeInputComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CodeInputComponent ]
     }).compileComponents();
